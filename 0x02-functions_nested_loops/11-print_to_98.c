cc58283770757f08cac54dae650e523f64315743
Mella5kk <onyeuukwunnaemeka@gmail.com>
2022-12-13 11:45:18 -0800

    Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,7 +5,6 @@
  * print_to_98 - prints all natural numbers to 98
  *
  * @n: number provided by user
- *
  */
 
 void print_to_98(int n)
@@ -19,7 +18,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (n = n, n > 98; n--)
+		for (n = n; n > 98; n--)
 			printf("%d, ", n);
 		printf("%d\n", 98);
 	}
