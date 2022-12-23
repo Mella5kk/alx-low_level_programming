d60cfa1082f7d79ad58c62eaedc05555da8e8171
Mella5kk <onyeuukwunnaemeka@gmail.com>
2022-12-23 02:41:36 -0800

    plus tasks

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,6 @@
 #include "main.h"
-
 /**
- * *string_toupper - changes all lowercase letters of a string to uppercase
+ * string_toupper - changes all lowercase letters of a string to uppercase
  * @word: Pointer to string
  * Return: Pointer to uppercase string
  */
